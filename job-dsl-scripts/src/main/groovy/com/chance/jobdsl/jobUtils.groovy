20617e3eb08cebb16b977512c6b01e7486a49202
Chance <chance.chen21@gmail.com>
2020-03-25 19:31:42 -0400

    Update JobUtil

--- job-dsl-scripts/src/main/groovy/com/chance/jobdsl/jobUtils.groovy
+++ job-dsl-scripts/src/main/groovy/com/chance/jobdsl/jobUtils.groovy
@@ -5,7 +5,7 @@ class JobUtils {
     static void addGitRepo_chance(context, String the_branch, String creds) {
         context.with {
             git {
-                branch('develop')
+                branch('master')
                 remote {
                     url('https://github.com/banyejinghungui/jenkins_pipeline.git')
                     credentials('b6cba214-602b-4337-8799-3c1b6aa911a9')
